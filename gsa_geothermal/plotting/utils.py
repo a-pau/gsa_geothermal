3dee65b1476271322a573d7e6e6732225c15dd7e
Andrea <Andrea@dell-pl-7.chemeng.ucl.ac.uk>
2021-12-10 11:22:10 +0000

    reordered and archived scripts

--- gsa_geothermal/plotting/utils.py
+++ gsa_geothermal/plotting/utils.py
@@ -165,3 +165,21 @@ def prepare_df(df, option):
     df = df.set_index('index')
 
     return df
+
+
+def set_axlims(series, marginfactor):
+    """
+    Fix for a scaling issue with matplotlibs scatterplot and small values.
+    Takes in a pandas series, and a marginfactor (float).
+    A marginfactor of 0.2 would for example set a 20% border distance on both sides.
+    Output:[bottom,top]
+    To be used with .set_ylim(bottom,top)
+    """
+    minv = series.min()
+    maxv = series.max()
+    datarange = maxv - minv
+    border = abs(datarange * marginfactor)
+    maxlim = maxv + border
+    minlim = minv - border
+
+    return minlim, maxlim
